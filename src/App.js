940f5d8ab686df96b0c8c2e1b04c4634c4952b28
Bziczek7 <bzik.pavel@outlook.com>
2023-03-07 21:24:35 +0000

    reuting

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
       <div>
         <NavTabs />
         {/* Wrap Route elements in a Routes component */}
-        <Routes>
+        <Routes basename="final-challenge">
           {/* Define routes using the Route component to render different page components at different paths */}
           {/* Define a default route that will render the Home component */}
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="blog" element={<Blog />} />
+          <Route path="final-challenge/" element={<Home />} />
+          <Route path="final-challenge/about" element={<About />} />
+          <Route path="final-challenge/blog" element={<Blog />} />
           {/* Define a route that will have descendant routes */}
-          <Route path="contact/*" element={<Contact />} />
+          <Route path="final-challenge/contact/*" element={<Contact />} />
         </Routes>
       </div>
     </Router>
